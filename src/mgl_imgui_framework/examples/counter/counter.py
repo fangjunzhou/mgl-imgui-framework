3cec2dbe60ba62c1a24b84f1e2f41e0c214b6b9d
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-02-27 10:48:19 -0800

    Added ImGUI demo window and counter window

---
+++ src/mgl_imgui_framework/examples/counter/counter.py
@@ -0,0 +1,70 @@
+"""
+A classic counter demo.
+"""
+
+from collections.abc import Callable
+from imgui_bundle import imgui, imgui_ctx
+from reactivex import Observable
+from reactivex.subject import BehaviorSubject
+from mgl_imgui_framework.menu_item import MenuItem
+from mgl_imgui_framework.window import Window
+
+
+class CounterMenuItem(MenuItem):
+    # Window open state.
+    open: bool
+    # Change open status callback.
+    on_change: Callable[[bool], None]
+
+    def __init__(
+        self,
+        open: Observable[bool],
+        on_change: Callable[[bool],
+                            None]) -> None:
+        def set_open(open: bool):
+            self.open = open
+        open.subscribe(set_open)
+        self.on_change = on_change
+
+    def render(self, time: float, frame_time: float) -> None:
+        changed, new_open = imgui.menu_item("Counter Window", "", self.open)
+        if changed:
+            self.on_change(new_open)
+
+
+class CounterWindow(Window):
+    # Window open state.
+    open: bool
+    # Close window callback.
+    on_close: Callable[[], None]
+
+    count: BehaviorSubject[int] = BehaviorSubject(0)
+
+    def __init__(
+        self,
+        open: Observable[bool],
+        on_close: Callable[[],
+                           None]) -> None:
+        def set_open(open: bool):
+            self.open = open
+        open.subscribe(set_open)
+        self.on_close = on_close
+
+    def increase(self):
+        self.count.on_next(self.count.value + 1)
+
+    def clear(self):
+        self.count.on_next(0)
+
+    def render(self, time: float, frame_time: float) -> None:
+        if self.open:
+            with imgui_ctx.begin("Counter", self.open) as window:
+                if not window.opened:
+                    self.on_close()
+
+                imgui.text(f"Current count: {self.count.value}")
+
+                if imgui.button("Increase"):
+                    self.increase()
+                if imgui.button("Clear"):
+                    self.clear()
